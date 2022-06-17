bcb1854966d1b3d86425e32c1c9b84f19812e635
Sandra Tena <sandratena06@gmail.com>
2022-06-17 13:54:44 -0700

    Debug crust options by changing it from checkboxes to select menu and setting the conditions within prototype function to equal the crust type

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 function Pizza(toppings, crust, size) {
   this.toppingsArray = toppings;
-  this.crustArray = crust
+  this.crust = crust
   this.size = size;
 }
 
@@ -17,58 +17,58 @@ Pizza.prototype.pizzaPrice = function() {
   const toppings3Large = 35
   const thinCrust = 2
   const deepDish = 4
-  if (this.toppingsArray.length > 1 && this.crustArray.includes("regular") && this.size === "small") {
+  if (this.toppingsArray.length > 1 && this.crust === "regular" && this.size === "small") {
     return toppings3Small;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("regular")  && this.size === "small") {
+  else if (this.toppingsArray.length === 1 && this.crust === "regular"  && this.size === "small") {
     return toppings1Small;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("regular") && this.size === "medium") {
+  else if (this.toppingsArray.length === 1 && this.crust === "regular" && this.size === "medium") {
     return toppings1Medium;
   } 
-  else if (this.toppingsArray.length > 1 && this.crustArray.includes("regular") && this.size === "medium") {
+  else if (this.toppingsArray.length > 1 && this.crust === "regular" && this.size === "medium") {
     return toppings3Medium;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("regular") && this.size === "large") {
+  else if (this.toppingsArray.length === 1 && this.crust === "regular" && this.size === "large") {
     return toppings1Large;
   } 
-  else if (this.toppingsArray.length > 1 && this.crustArray.includes("regular") && this.size === "large") {
+  else if (this.toppingsArray.length > 1 && this.crust === "regular" && this.size === "large") {
     return toppings3Large;
   } 
-  else if (this.toppingsArray.length > 1 && this.crustArray.includes("thin") && this.size === "small") {
+  else if (this.toppingsArray.length > 1 && this.crust === "thin" && this.size === "small") {
     return toppings3Small + thinCrust;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("thin")  && this.size === "small") {
+  else if (this.toppingsArray.length === 1 && this.crust === "thin"  && this.size === "small") {
     return toppings1Small + thinCrust;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("thin") && this.size === "medium") {
+  else if (this.toppingsArray.length === 1 && this.crust === "thin" && this.size === "medium") {
     return toppings1Medium + thinCrust;
   } 
-  else if (this.toppingsArray.length > 1 && this.crustArray.includes("thin") && this.size === "medium") {
+  else if (this.toppingsArray.length > 1 && this.crust === "thin" && this.size === "medium") {
     return toppings3Medium + thinCrust;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("thin") && this.size === "large") {
+  else if (this.toppingsArray.length === 1 && this.crust === "thin" && this.size === "large") {
     return toppings1Large + thinCrust;
   } 
-  else if (this.toppingsArray.length > 1 && this.crustArray.includes("thin") && this.size === "large") {
+  else if (this.toppingsArray.length > 1 && this.crust === "thin" && this.size === "large") {
     return toppings3Large + thinCrust;
   } 
-  else if (this.toppingsArray.length > 1 && this.crustArray.includes("deep") && this.size === "small") {
+  else if (this.toppingsArray.length > 1 && this.crust === "deep" && this.size === "small") {
     return toppings3Small + deepDish;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("deep")  && this.size === "small") {
+  else if (this.toppingsArray.length === 1 && this.crust === "deep"  && this.size === "small") {
     return toppings1Small + deepDish;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("deep") && this.size === "medium") {
+  else if (this.toppingsArray.length === 1 && this.crust === "deep" && this.size === "medium") {
     return toppings1Medium + deepDish;
   } 
-  else if (this.toppingsArray.length > 1 && this.crustArray.includes("deep") && this.size === "medium") {
+  else if (this.toppingsArray.length > 1 && this.crust === "deep" && this.size === "medium") {
     return toppings3Medium + deepDish;
   } 
-  else if (this.toppingsArray.length === 1 && this.crustArray.includes("deep") && this.size === "large") {
+  else if (this.toppingsArray.length === 1 && this.crust === "deep" && this.size === "large") {
     return toppings1Large + deepDish;
   } 
-  else if (this.toppingsArray.length > 1 && this.crustArray.includes("deep") && this.size === "large") {
+  else if (this.toppingsArray.length > 1 && this.crust === "deep" && this.size === "large") {
     return toppings3Large + deepDish;
   }
   else {
@@ -87,15 +87,11 @@ $(document).ready(function() {
       const allToppings = $(this).val();
       toppings.push(allToppings);
     })
-    let crust = [];
-    $("input:checkbox[name=crust]:checked").each(function(){
-      const allCrusts = $(this).val();
-      crust.push(allCrusts);
-    })
+    const crust = $("Select#crust").val();
     console.log(crust);
     const pizzaSize = $("select#size").val();
-    let pizzaOrder = new Pizza(toppings, pizzaSize);
+    let pizzaOrder = new Pizza(toppings, crust, pizzaSize);
     let pizzaOrderPrice = pizzaOrder.pizzaPrice();
-    $("#pizzaPrice").append("<p>" + "Pizza Price:" + pizzaOrderPrice + "</p>");
+    $("#pizzaPrice").append("<p>" + "Pizza Price: $" + pizzaOrderPrice + "</p>");
   })
 });
